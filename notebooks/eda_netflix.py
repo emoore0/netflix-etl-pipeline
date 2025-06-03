7489b1176d93a122d2ed9456d6ba01e487011afe
emoore0 <emmanuelmoore0@gmail.com>
2025-06-03 10:29:32 +0100

    netflix 2015 project

--- notebooks/eda_netflix.py
+++ notebooks/eda_netflix.py
@@ -3,11 +3,16 @@ import pandas as pd
 # load data
 df = pd.read_csv('datasets/netflix_titles.csv')
 
-# Overview
-print(df.info())
-print(df['type'].value_counts())
-print(df['country'].value_counts(5))
+# Clean and filter
+df_clean = df.dropna(subset=['country','type','release_year']) # Drop all rows which have a null value in any of these columns
+df_movies = df_clean[(df_clean['type'] == 'Movie') & (df_clean['release_year'] >= 2015)].copy() # filters the dataframe for all rows where type coulumn has Movie and for all movies released after 2015
 
-# Save a cleaned version for Tableau
-df_clean = df.dropna(subset=['country','type','release_year']) # Removes the rows that contains NULL values
-df_clean.to_csv('outputs/netflix_cleaned.csv', index=False)
+# Select relevant columns
+df_movies = df_movies[['title', 'country', 'release_year', 'listed_in']]  
+df_movies.columns = ['Title', 'Country', 'Release Year', 'Genres']  # Rename the columns
+
+df_movies['Count'] = 1 # adds a count column
+
+# Save to outputs
+df_movies.to_csv('outputs/netflix_movies_clean_2015.csv', index=False) # No index coulmn in data i.e no column giving every row a number
+print("Cleaned data saved for Tableau (Post 2015)")
