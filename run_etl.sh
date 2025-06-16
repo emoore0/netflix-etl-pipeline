544c83ea984ee3d63d2058f18b5cb87ea3e00a49
emoore0 <emmanuelmoore0@gmail.com>
2025-06-16 14:06:49 +0100

    rewrite ETL python script

--- run_etl.sh
+++ run_etl.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 echo "Starting ETL process at $(date)"
 
+# Activates virtual environment
 source venv/bin/activate
 
 # Run pipeline
-python3 scripts/etl_from_csv.py
+python3 scripts/etl_sql_template.py
 
 echo "ETL process completed at $(date)"
